06022042700e7d3153c1f57652f7d43163224704
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2026-02-20 20:04:36 -0300

    fix: changing dotenv place

--- app/main/main.go
+++ app/main/main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"gitcrawler/app/impl/register"
+	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
 	server := http.Server{}
 	register.GetHandlers()
 	server.Addr = ":8080"
 	fmt.Println("Running server on " + server.Addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 }
